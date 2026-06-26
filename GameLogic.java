5358f3a52bb4f05065d6eb8b544465c1e4972337
HughMann635 <zahrangalib@gmail.com>
2026-06-26 16:50:59 -0400

    set up files and imported libraries

---
+++ GameLogic.java
@@ -0,0 +1,10 @@
+//Importing libraries
+import javax.swing.JPanel;
+import java.awt.*;
+import java.util.Random;
+import javax.swing.Timer;
+import java.awt.event.*;
+
+public class GameLogic {
+    
+}
--- GameWindow.java
+++ GameWindow.java
@@ -1,5 +1,13 @@
 import javax.swing.JFrame;
 
 public class GameWindow extends JFrame {
-    
+    GameWindow() {
+        this.add(new GameWindow());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
